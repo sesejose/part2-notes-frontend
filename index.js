6e9bef7fd0f17f3af2f6fabc949b67688ea04fe3
José <sesejose@gmail.com>
2025-11-12 15:31:56 +0100

    Removing hello World again!

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static("dist")); // Add this line to serve static files
+app.use(express.static("dist")); // Serve static files from dist
 
 let notes = [
   {

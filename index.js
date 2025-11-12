c12da64194714326aa8020a1133a1e3b76e9faae
José <sesejose@gmail.com>
2025-11-12 14:41:46 +0100

    Commenting Hello World in index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("dist")); // Add this line to serve static files
 
 let notes = [
   {
@@ -25,9 +26,9 @@ let notes = [
   },
 ];
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
+// app.get("/", (request, response) => {
+//   response.send("<h1>Hello World!</h1>");
+// });
 
 app.get("/api/notes", (request, response) => {
   response.json(notes);
